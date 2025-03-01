701697daffeb29ceaedeb9b12084aebe1bf1e763
Turtlelord26 <jltalbott47@gmail.com>
2025-03-01 00:40:51 -0800

    Parallelization prototype
    Should upload the function the series' map functions are referencing. This is an example of separation of concerns - this class should be where all Series parallelization decisions and functionality live.
    Haven't decided yet whether utility functions live in a separate top-level project or in Utilities namespaces associated with specific objects. Leaning toward the latter for now, to allow independent customization of thresholds.
    
     Changes to be committed:
    	new file:   Series/Utilities/Parallelization/ParallelizationUtilities.cs

---
+++ Series/Utilities/Parallelization/ParallelizationUtilities.cs
@@ -0,0 +1,26 @@
+﻿namespace Series.Utilities.Parallelization
+{
+    public static class ParallelizationUtilities
+    {
+        public const int PARALLELISM_THRESHOLD = 128;
+
+        public static bool ParallelCondition(int dataLength) => dataLength >= PARALLELISM_THRESHOLD;
+
+        internal static void ParallelizeIfEfficient(Action<int> action, int dataEndpoint, int startPoint = 0)
+        {
+            if (ParallelCondition(dataEndpoint))
+            {
+                Parallel.For(startPoint, dataEndpoint,
+                    (i) => action(i)
+                    );
+            }
+            else
+            {
+                for (int i = startPoint; i < dataEndpoint; i++)
+                {
+                    action(i);
+                }
+            }
+        }
+    }
+}
